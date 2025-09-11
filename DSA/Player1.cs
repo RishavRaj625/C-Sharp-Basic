ee839f5420812852da07554074c87f23f633d6d2
Rishav Raj <165421390+RishavRaj625@users.noreply.github.com>
2025-09-12 03:16:02 +0530

    Add files via upload

---
+++ DSA/Player1.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Player
+{
+    // public, private, protected
+    public string name = "Rishu raj";
+    public int age = 21;
+    // private int age = 25;
+    public void SetAge(int a)
+    {
+        age = a;
+    }
+}
